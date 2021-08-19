bfbf951a32067ce9b1cc7026da789d8da13c459e
Hymperalix <36744293+Hymperalix@users.noreply.github.com>
2021-08-19 13:24:43 +0100

    Add files via upload

---
+++ background.js
@@ -0,0 +1,10 @@
+var outlook = /^https?:\/\/outlook\.live\.com\/mail\/0\/inbox/
+var twitch = /^https?:\/\/www\.twitch\.tv\/(?!videos)/
+//var rxLookfor_list = /^https?:\/\/agirsupport\.freshdesk\.com\/a\/tickets\/filters\/.+/
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
+  // if (rxLookfor.test(changeInfo.url) || rxLookfor_list.test(changeInfo.url)) {
+  if (outlook.test(changeInfo.url) || twitch.test(changeInfo.url)) {
+    chrome.tabs.sendMessage(tabId, 'page-rendered')
+  }
+})
